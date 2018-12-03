205b3c062a5f0f0f4222d45a2b60c04c071f87a9
qqwqqk <1103237276@qq.com>
2018-12-03 18:16:33 +0800

    图片调整
    图片调整

--- script/main.js
+++ script/main.js
@@ -9,7 +9,8 @@
 
     //初始的一些设置
     document.getElementById('enter').value = sites[0].getElementsByTagName("url")[0].childNodes[0].nodeValue;
-    document.getElementById("site_show").src =  sites[0].getElementsByTagName("url")[0].childNodes[0].nodeValue;
+    document.getElementById("site_show_image").src =  sites[0].getElementsByTagName("image")[0].childNodes[0].nodeValue;
+    document.getElementById("site_show_image").style.backgroundColor =  sites[0].getElementsByTagName("color")[0].childNodes[0].nodeValue;
     document.getElementById('abstract').innerHTML = sites[0].getElementsByTagName("name")[0].childNodes[0].nodeValue;
 
     length.value = sites.length;
@@ -39,15 +40,15 @@ function InitLists( list_star ,list_length){
     document.getElementById("show_list_header3").value = sites[index3].getElementsByTagName("name")[0].childNodes[0].nodeValue;
     document.getElementById("show_list_header4").value = sites[index4].getElementsByTagName("name")[0].childNodes[0].nodeValue;
     //设置超连接文本
-    document.getElementById("show_list_content1").title = sites[index1].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_content2").title = sites[index2].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_content3").title = sites[index3].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_content4").title = sites[index4].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+    document.getElementById("show_list_image1").title = sites[index1].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+    document.getElementById("show_list_image2").title = sites[index2].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+    document.getElementById("show_list_image3").title = sites[index3].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+    document.getElementById("show_list_image4").title = sites[index4].getElementsByTagName("name")[0].childNodes[0].nodeValue;
     //设置超连接
-    document.getElementById("show_list_content1").href = sites[index1].getElementsByTagName("url")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_content2").href = sites[index2].getElementsByTagName("url")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_content3").href = sites[index3].getElementsByTagName("url")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_content4").href = sites[index4].getElementsByTagName("url")[0].childNodes[0].nodeValue;
+    document.getElementById("show_list_image1").alt = sites[index1].getElementsByTagName("url")[0].childNodes[0].nodeValue;
+    document.getElementById("show_list_image2").alt = sites[index2].getElementsByTagName("url")[0].childNodes[0].nodeValue;
+    document.getElementById("show_list_image3").alt = sites[index3].getElementsByTagName("url")[0].childNodes[0].nodeValue;
+    document.getElementById("show_list_image4").alt = sites[index4].getElementsByTagName("url")[0].childNodes[0].nodeValue;
     //设置图片背景
     document.getElementById("show_list_image1").style.backgroundColor = sites[index1].getElementsByTagName("color")[0].childNodes[0].nodeValue;
     document.getElementById("show_list_image2").style.backgroundColor = sites[index2].getElementsByTagName("color")[0].childNodes[0].nodeValue;
@@ -58,10 +59,9 @@ function InitLists( list_star ,list_length){
     document.getElementById("show_list_image2").src = sites[index2].getElementsByTagName("image")[0].childNodes[0].nodeValue;
     document.getElementById("show_list_image3").src = sites[index3].getElementsByTagName("image")[0].childNodes[0].nodeValue;
     document.getElementById("show_list_image4").src = sites[index4].getElementsByTagName("image")[0].childNodes[0].nodeValue;
-
-
 }
 
+//菜单选项平行左移
 function MoveLeft(){
     var list_star = document.getElementById("list_star");
     var list_length = document.getElementById("list_length");
@@ -73,6 +73,7 @@ function MoveLeft(){
     document.getElementById("list_star").value = star;
 }
 
+//菜单选项平行右移
 function MoveRight() {
     var list_star = document.getElementById("list_star");
     var list_length = document.getElementById("list_length");
@@ -83,27 +84,45 @@ function MoveRight() {
     InitLists(star,length);
     document.getElementById("list_star").value = star;
 }
+
+//单击第一个选项框
 function Clicked1(){
-    var info = document.getElementById("show_list_content1");
+    var info = document.getElementById("show_list_image1");
     document.getElementById('abstract').innerHTML = info.title;
-    document.getElementById('enter').value = info.href;
+    document.getElementById('enter').value = info.alt;
+    document.getElementById("site_show_image").src =  info.src;
+    document.getElementById("site_show_image").style.backgroundColor =  info.style.backgroundColor;
+    return false;
 }
+//单击第二个选项框
 function Clicked2(){
-    var info = document.getElementById("show_list_content2");
+    var info = document.getElementById("show_list_image2");
     document.getElementById('abstract').innerHTML = info.title;
-    document.getElementById('enter').value = info.href;
+    document.getElementById('enter').value = info.alt;
+    document.getElementById("site_show_image").src =  info.src;
+    document.getElementById("site_show_image").style.backgroundColor =  info.style.backgroundColor;
+    return false;
 }
+//单击第三个选项框
 function Clicked3(){
-    var info = document.getElementById("show_list_content3");
+    var info = document.getElementById("show_list_image3");
     document.getElementById('abstract').innerHTML = info.title;
-    document.getElementById('enter').value = info.href;
+    document.getElementById('enter').value = info.alt;
+    document.getElementById("site_show_image").src =  info.src;
+    document.getElementById("site_show_image").style.backgroundColor =  info.style.backgroundColor;
+    return false;
 }
+//单击第四个选项框
 function Clicked4(){
-    var info = document.getElementById("show_list_content4");
+    var info = document.getElementById("show_list_image4");
     document.getElementById('abstract').innerHTML = info.title;
-    document.getElementById('enter').value = info.href;
+    document.getElementById('enter').value = info.alt;
+    document.getElementById("site_show_image").src =  info.src;
+    document.getElementById("site_show_image").style.backgroundColor =  info.style.backgroundColor;
+    return false;
 }
 
+//打开新的窗口进行浏览
 function Browse(){
     var url = document.getElementById("enter").value;
     window.open(url, '_blank');
