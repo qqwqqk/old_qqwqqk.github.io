198a6df1694dcb33b11d56dd00a8084f04d2d054
qqwqqk <1103237276@qq.com>
2019-02-19 16:58:36 +0800

    alchemy
    框架调整

--- URL_Alchemy/script/alchemy.js
+++ URL_Alchemy/script/alchemy.js
@@ -1,72 +1,135 @@
 window.onload = function(){
+    var star = document.getElementById("list_star");
     var length = document.getElementById("list_length");
-
-    if (window.XMLHttpRequest) { xmlhttp = new XMLHttpRequest(); } else { xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); }
-    xmlhttp.open("GET", "xmls/alchemy.xml", false);
-    xmlhttp.send();
-    xmlDoc = xmlhttp.responseXML;
-    var sites = xmlDoc.getElementsByTagName("site_node");
-
-    document.getElementById('choice_value').value = 1 ;     //默认选择为1
-
-    //初始的一些设置
-    document.getElementById('title').value = sites[0].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-    document.getElementById("show_role").style.backgroundColor =  sites[0].getElementsByTagName("color")[0].childNodes[0].nodeValue;
-    document.getElementById("show_role").src =  sites[0].getElementsByTagName("role")[0].childNodes[0].nodeValue;
-    document.getElementById("show_doll").src =  sites[0].getElementsByTagName("doll")[0].childNodes[0].nodeValue;
-    document.getElementById("show_plot").src =  sites[0].getElementsByTagName("plot")[0].childNodes[0].nodeValue;
-    document.getElementById('Introduction').innerHTML = sites[0].getElementsByTagName("description")[0].childNodes[0].nodeValue;
-
-    //alert(sites[0].getElementsByTagName("description")[0].childNodes[0].nodeValue);
-
-    length.value = sites.length;
-    InitLists(0 , length.value );
-    ChoiceShow();
+    star.value = 0;
+    length.value = InitListFull(9).value;
+    ClickedList(0);
+    Choice1();
 }
 
-function InitLists( list_star ,list_length){
+//页面初始设置
+function InitListFull(list_count){
     if (window.XMLHttpRequest) { xmlhttp = new XMLHttpRequest(); } else { xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); }
-    xmlhttp.open("GET", "xmls/alchemy.xml", false);
+    xmlhttp.open("GET",  "xmls/alchemy.xml", false);
     xmlhttp.send();
     xmlDoc = xmlhttp.responseXML;
     var sites = xmlDoc.getElementsByTagName("site_node");
 
-    var star = parseInt(list_star);
-    var length = parseInt(list_length);
+    for(var i=0; i<list_count; i++){
+        var id = i;
+        var name = sites[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+        var color = sites[i].getElementsByTagName("color")[0].childNodes[0].nodeValue;
+        var img = sites[i].getElementsByTagName("icon")[0].childNodes[0].nodeValue;
+        var text = sites[i].getElementsByTagName("description")[0].childNodes[0].nodeValue;
+        AddLists(id, name, color, img, text, 1);
+    }
+    for(var j=list_count; j<sites.length; j++){
+        var p_id = j;
+        var p_name = sites[j].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+        var p_color = sites[j].getElementsByTagName("color")[0].childNodes[0].nodeValue;
+        var p_img = sites[j].getElementsByTagName("icon")[0].childNodes[0].nodeValue;
+        var p_text = sites[j].getElementsByTagName("description")[0].childNodes[0].nodeValue;
+        AddLists(p_id, p_name, p_color, p_img, p_text, 0);
+    }
+    for(var k=0; k<list_count - 1; k++){
+        var pr_id = k + sites.length;
+        var pr_name = sites[k].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+        var pr_color = sites[k].getElementsByTagName("color")[0].childNodes[0].nodeValue;
+        var pr_img = sites[k].getElementsByTagName("icon")[0].childNodes[0].nodeValue;
+        var pr_text = sites[k].getElementsByTagName("description")[0].childNodes[0].nodeValue;
+        AddLists(pr_id, pr_name, pr_color, pr_img, pr_text, 0);
+    }
 
+    var count = new Object();
+    count.value = sites.length;
+    return count;
+}
+//添加图标列表
+function AddLists(id, name, color, img, text, show){
+    var newDiv = document.createElement("div");
+    var str = 'listshow' + id;
+    newDiv.id = str;
+    newDiv.className = "show_list_box";
+    newDiv.innerHTML="<img id=\'listimg" + id +"\' title=\'"+ name +"\' src=\'"+ img +"\' alt=\'"+ text +"\' style=\'background-color:"+ color +" \' class=\'show_list_content\' onclick=\'ClickedList("+ id +")\'>";
+    document.getElementById("list_td").appendChild(newDiv);
+    if(!show){ document.getElementById(str).style.display = "none"; }
+}
+//菜单选项平行右移
+function MoveRight(){
+    var list_star = document.getElementById("list_star");
+    var list_length = document.getElementById("list_length");
+    var star = parseInt(list_star.value) + 1;
+    var length = parseInt(list_length.value);
+
+    if(star >= length){
+        for(var i = parseInt(list_star.value); i<length+8; i++){
+            var str1 = 'listshow' + i;
+            document.getElementById(str1).style.display = "none";
+        }
+        star = star - length;
+        for(var j = star; j<9;j++){
+            var str2 = 'listshow' + j;
+            document.getElementById(str2).style.display = "block";
+        }
+    }
+    else{
+        var str3 = 'listshow' + parseInt(list_star.value);
+        document.getElementById(str3).style.display = "none";
+        var cache = parseInt(list_star.value) + 9;
+        var str4 = 'listshow' + cache;
+        document.getElementById(str4).style.display = "block";
+    }
     document.getElementById("list_star").value = star;
-
-    var index0 = star;
-    var index1 = (star + 1) % length;
-    var index2 = (star + 2) % length;
-    var index3 = (star + 3) % length;
-    var index4 = (star + 4) % length;
-    var index5 = (star + 5) % length;
-    var index6 = (star + 6) % length;
-    var index7 = (star + 7) % length;
-    var index8 = (star + 8) % length;
-
-    //设置图片背景
-    document.getElementById("show_list_image0").style.backgroundColor = sites[index0].getElementsByTagName("color")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image1").style.backgroundColor = sites[index1].getElementsByTagName("color")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image2").style.backgroundColor = sites[index2].getElementsByTagName("color")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image3").style.backgroundColor = sites[index3].getElementsByTagName("color")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image4").style.backgroundColor = sites[index4].getElementsByTagName("color")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image5").style.backgroundColor = sites[index5].getElementsByTagName("color")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image6").style.backgroundColor = sites[index6].getElementsByTagName("color")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image7").style.backgroundColor = sites[index7].getElementsByTagName("color")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image8").style.backgroundColor = sites[index8].getElementsByTagName("color")[0].childNodes[0].nodeValue;
-    //设置图片内容
-    document.getElementById("show_list_image0").src = sites[index0].getElementsByTagName("icon")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image1").src = sites[index1].getElementsByTagName("icon")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image2").src = sites[index2].getElementsByTagName("icon")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image3").src = sites[index3].getElementsByTagName("icon")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image4").src = sites[index4].getElementsByTagName("icon")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image5").src = sites[index5].getElementsByTagName("icon")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image6").src = sites[index6].getElementsByTagName("icon")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image7").src = sites[index7].getElementsByTagName("icon")[0].childNodes[0].nodeValue;
-    document.getElementById("show_list_image8").src = sites[index8].getElementsByTagName("icon")[0].childNodes[0].nodeValue;
 }
+//菜单选项平行左移
+function MoveLeft() {
+    var list_star = document.getElementById("list_star");
+    var list_length = document.getElementById("list_length");
+    var star = parseInt(list_star.value) - 1;
+    var length = parseInt(list_length.value);
+
+    if(star < 0){
+        for(var i = 0; i<9; i++){
+            var str1 = 'listshow' + i;
+            document.getElementById(str1).style.display = "none";
+        }
+        star = star + length;
+        for(var j = star; j<length+8; j++){
+            var str2 = 'listshow' + j;
+            document.getElementById(str2).style.display = "block";
+        }
+    }
+    else{
+        var cache = parseInt(list_star.value) + 8;
+        var str3 = 'listshow' + cache;
+        document.getElementById(str3).style.display = "none";
+        var str4 = 'listshow' + star;
+        document.getElementById(str4).style.display = "block";
+    }
+    document.getElementById("list_star").value = star;
+}
+//单击图标列表响应函数
+function ClickedList(id){
+    var list_length = document.getElementById("list_length");
+    var length = parseInt(list_length.value);
+    var list_index = parseInt(id);
+    if(list_index >= length){list_index = list_index - length};
+
+    var index = 'listimg' + list_index;
+    var info = document.getElementById(index);
+    document.getElementById('title').value = info.title;
+    document.getElementById("show_role").style.backgroundColor =  info.style.backgroundColor;
+    document.getElementById('Introduction').innerHTML = info.alt;
+
+    var str = info.src;
+    var role_src = str.replace(/icon/g, "role");
+    var doll_src = str.replace(/icon/g, "doll");
+    var plot_src = str.replace(/icon/g, "plot");
+    document.getElementById("show_role").src = role_src;
+    document.getElementById("show_doll").src = doll_src;
+    document.getElementById("show_plot").src = plot_src;
+}
+
 function ChoiceShow(){
     var cache = document.getElementById("choice_value");
     var choice = parseInt(cache.value);
@@ -87,21 +150,6 @@ function ChoiceShow(){
         document.getElementById('site_show3').style.display = "block";
     }
 }
-function SiteShow(number){
-    if (window.XMLHttpRequest) { xmlhttp = new XMLHttpRequest(); } else { xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); }
-    xmlhttp.open("GET", "xmls/alchemy.xml", false);
-    xmlhttp.send();
-    xmlDoc = xmlhttp.responseXML;
-    var sites = xmlDoc.getElementsByTagName("site_node");
-
-    document.getElementById('title').value = sites[number].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-    document.getElementById("show_role").style.backgroundColor =  sites[number].getElementsByTagName("color")[0].childNodes[0].nodeValue;
-    document.getElementById("show_role").src =  sites[number].getElementsByTagName("role")[0].childNodes[0].nodeValue;
-    document.getElementById("show_doll").src =  sites[number].getElementsByTagName("doll")[0].childNodes[0].nodeValue;
-    document.getElementById("show_plot").src =  sites[number].getElementsByTagName("plot")[0].childNodes[0].nodeValue;
-    document.getElementById('Introduction').innerHTML = sites[number].getElementsByTagName("description")[0].childNodes[0].nodeValue;
-}
-
 function Choice1() {
     document.getElementById('choice_value').value = 1 ;
     ChoiceShow();
@@ -116,85 +164,6 @@ function Choice3() {
     document.getElementById('choice_value').value = 3 ;
     ChoiceShow();
     return 0;
-}
-
-//菜单选项平行左移
-function MoveLeft(){
-    var list_star = document.getElementById("list_star");
-    var list_length = document.getElementById("list_length");
-    var star = parseInt(list_star.value) - 1;
-    var length = parseInt(list_length.value);
-
-    if(star < 0){ star = star + length; }
-    InitLists(star,length);
-    document.getElementById("list_star").value = star;
-}
-
-//菜单选项平行右移
-function MoveRight() {
-    var list_star = document.getElementById("list_star");
-    var list_length = document.getElementById("list_length");
-    var star = parseInt(list_star.value) + 1;
-    var length = parseInt(list_length.value);
-
-    if(star >= length){ star = star - length; }
-    InitLists(star,length);
-    document.getElementById("list_star").value = star;
-}
-
-//单击列表选项框
-function Clicked0(){
-    var star = document.getElementById("list_star").value;
-    var site = parseInt(star);
-    SiteShow(site);
-}
-function Clicked1(){
-    var star = document.getElementById("list_star").value;
-    var length = document.getElementById("list_length").value;
-    var site = (parseInt(star) + 1) % parseInt(length);
-    SiteShow(site);
-}
-function Clicked2(){
-    var star = document.getElementById("list_star").value;
-    var length = document.getElementById("list_length").value;
-    var site = (parseInt(star) + 2) % parseInt(length);
-    SiteShow(site);
-}
-function Clicked3(){
-    var star = document.getElementById("list_star").value;
-    var length = document.getElementById("list_length").value;
-    var site = (parseInt(star) + 3) % parseInt(length);
-    SiteShow(site);
-}
-function Clicked4(){
-    var star = document.getElementById("list_star").value;
-    var length = document.getElementById("list_length").value;
-    var site = (parseInt(star) + 4) % parseInt(length);
-    SiteShow(site);
-}
-function Clicked5(){
-    var star = document.getElementById("list_star").value;
-    var length = document.getElementById("list_length").value;
-    var site = (parseInt(star) + 5) % parseInt(length);
-    SiteShow(site);
-}
-function Clicked6(){
-    var star = document.getElementById("list_star").value;
-    var length = document.getElementById("list_length").value;
-    var site = (parseInt(star) + 6) % parseInt(length);
-    SiteShow(site);
-}
-function Clicked7(){
-    var star = document.getElementById("list_star").value;
-    var length = document.getElementById("list_length").value;
-    var site = (parseInt(star) + 7) % parseInt(length);
-    SiteShow(site);
-}
-function Clicked8(){
-    var star = document.getElementById("list_star").value;
-    var length = document.getElementById("list_length").value;
-    var site = (parseInt(star) + 8) % parseInt(length);
-    SiteShow(site);
 }
 
 //打开新的窗口进行浏览
