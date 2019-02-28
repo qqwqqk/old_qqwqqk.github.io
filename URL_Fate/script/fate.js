a9d7cafa4dcf526c121912d55496548890afe032
qqwqqk <1103237276@qq.com>
2019-02-28 09:34:19 +0800

    control_test
    进度条控制测试

---
+++ URL_Fate/script/fate.js
@@ -0,0 +1,11 @@
+
+
+//菜单选项平行右移
+function MoveRight(){
+
+}
+
+//菜单选项平行左移
+function MoveLeft() {
+
+}
--- URL_Fate/script/video_control.js
+++ URL_Fate/script/video_control.js
@@ -1,8 +1,19 @@
-var player = videojs('media', {
-    muted: true,
+var options = {
+    autoplay: true,
     controls: true,
-    height: 300,
-    width: 400,
-    loop: true,
-    // 更多配置.....
+    loop: false,
+    muted: false,
+    playbackRates: [0.5, 1, 1.5, 2]
+};
+
+var player = videojs('media', options, function onPlayerReady() {
+    videojs.log('播放器已经准备好了!');
+
+    // In this context, `this` is the player that was created by Video.js.<br>  // 注意，这个地方的上下文， `this` 指向的是Video.js的实例对像player
+    //this.play();
+
+    // How about an event listener?<br>  // 如何使用事件监听？
+    this.on('ended', function() {
+        videojs.log('播放结束了!');
+    });
 });
